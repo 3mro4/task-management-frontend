c156471d0a0700a063f52fb29103843e1cd70d4e
Reef Khalid Aljasser <rkaljaser@trainee.tcc-ict.com>
2026-04-01 11:23:52 +0300

    Add task detail page

--- src/app/features/tasks/task-detail/task-detail.ts
+++ src/app/features/tasks/task-detail/task-detail.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from '../../../core/services/task.service';
+import { TaskDto } from '../../../core/models/task';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-task-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './task-detail.html',
-  styleUrl: './task-detail.css',
+  styleUrl: './task-detail.css'
 })
-export class TaskDetail {}
+export class TaskDetail implements OnInit {
+
+  private route = inject(ActivatedRoute);
+  private taskService = inject(TaskService);
+
+  task: TaskDto | null = null;
+  isLoading = false;
+  errorMessage = '';
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.loadTask(id);
+    } else {
+      this.errorMessage = 'Task id is missing.';
+    }
+  }
+
+  loadTask(id: string): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.taskService.getById(id).subscribe({
+      next: (res: TaskDto) => {
+        this.task = res;
+        this.isLoading = false;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err);
+        this.errorMessage = 'Failed to load task details.';
+        this.isLoading = false;
+      }
+    });
+  }
+}
