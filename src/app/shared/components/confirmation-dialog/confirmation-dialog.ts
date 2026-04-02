124f5dca845908c706b0648b91ade29bb15de07b
Reef Khalid Aljasser <rkaljaser@trainee.tcc-ict.com>
2026-04-02 11:11:08 +0300

    Add shared confirmation dialog component

--- src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-confirmation-dialog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './confirmation-dialog.html',
-  styleUrl: './confirmation-dialog.css',
+  styleUrl: './confirmation-dialog.css'
 })
-export class ConfirmationDialog {}
+export class ConfirmationDialog {
+  @Input() isVisible = false;
+  @Input() title = 'Confirm Action';
+  @Input() message = 'Are you sure you want to continue?';
+
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  onConfirm(): void {
+    this.confirm.emit();
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
+  }
+}
