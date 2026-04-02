7d9f18fe45aa7a4f0c95871dedccca18dd208c11
Reef Khalid Aljasser <rkaljaser@trainee.tcc-ict.com>
2026-04-02 13:11:01 +0300

    UI: improved the  design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, signal, inject } from '@angular/core';
+import { filter } from 'rxjs';  
+import { CommonModule } from '@angular/common'; 
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  private router = inject(Router);
+  showNavbar = true;
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.updateNavbarVisibility();
+    });
+    this.updateNavbarVisibility();
+  }
+
+  private updateNavbarVisibility(): void {
+    const currentUrl = this.router.url;
+    this.showNavbar = !['/login', '/register'].includes(currentUrl);
+  }
   protected readonly title = signal('task-management-frontend');
 }
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject} from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  private router = inject(Router);
+
+  logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  }
+  
+}
+
